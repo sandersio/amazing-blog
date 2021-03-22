f586fd2bde612ddbbb515ab750eb778796f8004c
Rahul Mehra <rahul.mehraa.18@gmail.com>
2021-03-22 10:29:18 +0000

    bio.js: updated Paragraph

--- src/components/bio.js
+++ src/components/bio.js
@@ -53,7 +53,7 @@ const Bio = () => {
       )}
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Written by asdasd <strong>{author.name}</strong> {author?.summary || null}
           {` `}
           <a href={`https://twitter.com/${social?.twitter || ``}`}>
             You should follow them on Twitter
